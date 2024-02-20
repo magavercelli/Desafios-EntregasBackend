004f67f6e6ef8496b772379d51eedeea26ca64cc
magavercelli <magabrielavercelli@hotmail.com>
2024-02-20 20:09:21 -0300

    solucionado sessions

--- src/routes/sessions.route.js
+++ src/routes/sessions.route.js
@@ -4,18 +4,38 @@ import { SessionsController } from '../controllers/sessions.controller.js';
 
 const router = Router();
 
-router.post('/register', passport.authenticate('register', { failureRedirect: '/api/sessions/failregister' }), SessionsController.register);
-router.post('/login', passport.authenticate('login', { failureRedirect: '/api/sessions/faillogin' }), SessionsController.login);
-router.post('/restartPassword', SessionsController.restartPassword);
+router.post('/register', passport.authenticate('register', {failureRedirect: '/api/sessions/failregister'}),
+async (req,res)=> {
+    res.send({
+        status: 'success', 
+        message: 'User created successfully'
+    })
+})
 
 router.get('/failregister', (req, res) => {
     console.log('Registration has failed');
     res.send({ error: "Registration failed!" });
 });
 
+router.post('/login', passport.authenticate('login', {
+    failureRedirect: '/api/sessions/faillogin'}),
+async(req,res)=> {
+    if(!req.user){
+        return res.status(400).send({status:'error'})
+    }
+    req.session.user ={
+        first_name: req.user.first_name,
+        last_name: req.user.last_name,
+        age: req.user.age,
+        email:req.user.email
+    }
+    res.send({status: 'success', payload: req.user})
+})
+
 router.get('/faillogin', (req, res) => {
     res.send({ error: 'fail login' });
 });
+router.post('/restartPassword', SessionsController.restartPassword);
 
 router.get('/github', passport.authenticate('github', { scope: ['user:email'] }), async (req, res) => {});
 router.get('/githubcallback', passport.authenticate('github', { failureRedirect: '/login' }), async (req, res) => {
--- src/services/sessions.services.js
+++ src/services/sessions.services.js
@@ -2,18 +2,18 @@ import userModel from '../dao/models/user.model.js';
 import { createHash } from '../utils.js';
 
 class SessionsService {
-    static restartPassword = (email, password)=> {
+    static restartPassword = async (email, password) => {
         if (!email || !password) {
             throw new Error('Invalid email or password');
         }
 
-        const user =  userModel.findOne({ email });
+        const user = await userModel.findOne({ email });
         if (!user) {
             throw new Error('User not found');
         }
 
         const newHashPassword = createHash(password);
-         userModel.updateOne({ _id: user._id }, { $set: { password: newHashPassword } });
+        await userModel.updateOne({ _id: user._id }, { $set: { password: newHashPassword } });
         return { status: 'success', message: 'Password reset successfully' };
     }
 }
